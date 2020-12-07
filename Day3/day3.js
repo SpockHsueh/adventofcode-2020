1e461ea2c63a00742b1043c67d71a924cae22dc5
Spock <>
2020-12-07 18:40:46 +0800

    Day 3-1 finish

--- Day3/day3.js
+++ Day3/day3.js
@@ -1,23 +1,37 @@
 var fs = require('fs');
 let data = fs.readFileSync('day3info.txt').toString().split("\n");
 
-function myFunction(data) {
-  let count = 3
+function myFunction(data, rightCount, downCount) {
+  let count = rightCount
   let res = 0
-  for (let i = 1; i < data.length; i++) {
+  for (let i = downCount ; i < data.length; i+=downCount) {
     const item = data[i]
     const rowLength = data[i].length
     let targetNum = count % rowLength
-    const targetLocation = item[targetNum]
+    const targetLocation = item[targetNum]        
     if (targetLocation == "#") {
       res += 1
     }
-    count += 3
+    count += rightCount
   }
   return res
-
 }
 
+// console.log(myFunction(data, 1, 2))
 
-console.log(myFunction(data))
-console.log(62 % 31);
+function myFunction2(data) {
+  const slopes = [{1:1},{3:1}, {5:1}, {7:1}, {1:2}]
+  let res = 1
+  for(let i = 0; i < slopes.length; i++) {
+    const key = slopes[i]
+    Object.keys(key).forEach(element => {
+      const rightCount = parseInt(element)
+      const downCount = parseInt(key[element])
+      const eachSlopeTrees = myFunction(data, rightCount, downCount)
+      res *= eachSlopeTrees
+    }); 
+  }
+  return res
+}
+
+console.log(myFunction2(data))
