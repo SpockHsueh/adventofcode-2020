7eff6e5c75a10736af6369933db44eb7954824a5
Spock <>
2020-12-11 18:29:30 +0800

    Starting day5

---
+++ Day5/day5.js
@@ -0,0 +1,15 @@
+const data = ["FBFBBFFRLR"]
+
+function myFunction(data) {
+  
+  for (const i of data) {
+
+    for (const e of i) {   
+      let lowerBound = 0
+      let upperBound = 127   
+      if (e == f) 
+    }
+  }
+}
+
+myFunction(data)
