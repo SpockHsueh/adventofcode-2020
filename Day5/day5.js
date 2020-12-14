8b119d4e3f0d39223b742e585f3ed5d827ef088f
Spock <>
2020-12-14 12:28:57 +0800

    Finish Day5

--- Day5/day5.js
+++ Day5/day5.js
@@ -1,15 +1,49 @@
-const data = ["FBFBBFFRLR"]
+var fs = require('fs');
+let data = fs.readFileSync('day5info.txt').toString().split("\n");
 
 function myFunction(data) {
-  
-  for (const i of data) {
-
-    for (const e of i) {   
-      let lowerBound = 0
-      let upperBound = 127   
-      if (e == f) 
+  let res = 0
+  let seatIds = []
+  for (const x of data) {
+    let row
+    let column
+    let seatId
+    let lowerBound = 0
+    let upperBound = 127
+    let columnLowerBound = 0
+    let columnUpperBound = 7
+    for (let index = 0; index < x.length; index ++) {
+      const item = x[index]
+      if (index <= 6) {
+        if (item == 'F') {
+          upperBound = lowerBound + Math.floor((upperBound - lowerBound) / 2)
+        } 
+        if (item == 'B') {
+          lowerBound = upperBound - Math.floor((upperBound - lowerBound) / 2)
+        }
+      }
+      row = lowerBound
+      if (index > 6) {
+        if (item == 'R') {
+          columnLowerBound = columnUpperBound - Math.floor((columnUpperBound - columnLowerBound) / 2)
+        } else if (item == 'L') {
+          columnUpperBound = columnLowerBound + Math.floor((columnUpperBound - columnLowerBound) / 2)
+        }
+      }
+      column = columnUpperBound      
     }
+    seatId = row * 8 + column
+    seatIds.push(seatId)
+
+    res = seatId > res ? seatId : res
+    seatIds.push(res)
   }
+  seatIds.sort(function(a,b) {
+    return a-b
+  }).filter(function(id, i, array) {
+    if (array[i+1] - id > 1 ) {console.log(id + 1);}
+  } )  
+  return res
 }
 
 myFunction(data)
